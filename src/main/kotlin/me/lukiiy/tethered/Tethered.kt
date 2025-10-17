502547577fa942156e5be48394fb7c1f7aab7e0d
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2025-10-17 14:29:59 -0300

    1.1
    ok i tried

--- src/main/kotlin/me/lukiiy/tethered/Tethered.kt
+++ src/main/kotlin/me/lukiiy/tethered/Tethered.kt
@@ -8,13 +8,12 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerKickEvent
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.function.Consumer
 
 class Tethered : JavaPlugin(), Listener {
-    var maxDist = 6.0
-    var dragDist = maxDist / 2
-    var maxGroup = 2
+    var dragDist = 4.0
 
     override fun onEnable() {
         setupConfig()
@@ -39,39 +38,31 @@ class Tethered : JavaPlugin(), Listener {
     }
 
     fun loadVariables() {
-        maxDist = config.getDouble("max_distance")
-        dragDist = config.getDouble("drag_distance")
-        maxGroup = config.getInt("max_group_size")
+        dragDist = config.getDouble("dragDistance", dragDist)
     }
 
     // Stuff
     private val tickTask: Consumer<ScheduledTask> = Consumer { task ->
-        for (player in server.onlinePlayers.filter(isValidPlayer)) {
-            val others = getGroupPlayers(player)
-            if (others.isEmpty()) continue
+        val players = server.onlinePlayers.filter { isValidPlayer(it) }
+        if (players.isEmpty()) return@Consumer
 
+        for (player in players) {
+            val others = getNearestPlayer(player)?.let { listOf(it) } ?: continue
             val group = listOf(player) + others
 
-            val center = group.map { it.location.toVector() }
-                .reduce { acc, vec -> acc.add(vec) }
-                .multiply(1.0 / group.size)
+            val center = group.map { it.location.toVector() }.reduce { acc, vec -> acc.add(vec) }.multiply(1.0 / group.size)
 
             for (p in group) {
-                val pLocation = p.location.toVector()
-                val dist = pLocation.distance(center)
+                val pVec = p.location.toVector()
+                val dist = pVec.distance(center)
+                if (dist <= dragDist) continue
 
-                if (dist > dragDist) {
-                    val distRelative = (dist - dragDist) / (maxDist - dragDist)
-                    val speed = .5 * distRelative.coerceIn(.75, 2.0)
+                var speed = .5 * ((dist - dragDist) / dragDist).coerceIn(0.75, 2.0)
 
-                    if (p.isInsideVehicle) p.leaveVehicle()
-                    p.velocity = center.clone().subtract(pLocation).normalize().multiply(speed).apply { y += 0.1 }
+                if (p.isInsideVehicle) p.leaveVehicle()
 
-                    for (other in group) {
-                        if (other == p || listOf(p, other).random() != p) continue // Choose 1 to draw
-                        rayParticle(p.location.add(0.0, p.boundingBox.height / 2, 0.0), other.location.add(0.0, other.boundingBox.height / 2, 0.0))
-                    }
-                }
+                p.velocity = center.clone().subtract(pVec).normalize().multiply(speed).apply { y += if (p.location.y < player.location.y) 0.1 else 0.0 }
+                if (p.world.fullTime % 2L == 0L) rayParticle(p.location.add(0.0, p.boundingBox.height / 2, 0.0), player.location.add(0.0, player.boundingBox.height / 2, 0.0))
             }
         }
     }
@@ -79,7 +70,7 @@ class Tethered : JavaPlugin(), Listener {
     fun rayParticle(from: Location, to: Location) {
         val rayDir = to.toVector().subtract(from.toVector())
         val stepVec = rayDir.clone().normalize().multiply(.5)
-        val steps = (rayDir.length() * 2.0).toInt().coerceAtMost(25)
+        val steps = (rayDir.length() * 3.0).toInt().coerceAtMost(25)
 
         var point = from.toVector()
         repeat(steps) {
@@ -88,24 +79,26 @@ class Tethered : JavaPlugin(), Listener {
         }
     }
 
-    val isValidPlayer = { p: Player -> !p.gameMode.isInvulnerable && !p.isDead }
+    private fun getNearestPlayer(p: Player): Player? {
+        val pLoc = p.location
 
-    fun getGroupPlayers(player: Player): List<Player> {
-        return player.world.players.asSequence()
-            .filter { it != player && isValidPlayer(it) }
-            .sortedBy { it.location.distanceSquared(player.location) }
-            .take(maxGroup - 1)
-            .toList()
+        return p.world.players.asSequence().filter { it != p && isValidPlayer(it) }.minByOrNull { it.location.distanceSquared(pLoc) }
     }
+
+    val isValidPlayer = { p: Player -> !p.gameMode.isInvulnerable && !p.isDead }
 
     @EventHandler
     fun death(e: PlayerDeathEvent) {
-        val player = e.player
-        if (player.gameMode.isInvulnerable) return
+        val p = e.player
+        if (p.gameMode.isInvulnerable) return
 
-        val group = getGroupPlayers(player)
-        if (group.isEmpty()) return
+        e.player.scheduler.run(this, {
+            getNearestPlayer(p)?.damage(999.0, e.damageSource)
+        }, null)
+    }
 
-        for (player in group) player.health = 0.0
+    @EventHandler
+    fun kick(e: PlayerKickEvent) {
+        if (e.cause == PlayerKickEvent.Cause.FLYING_PLAYER) e.isCancelled = true
     }
 }
